ef70b3320a2bf38be4106ca7195e14133218ef15
Sam Chai <sam.chai0501@gmail.com>
2018-05-05 15:41:46 -0400

    express routes

--- server/app.js
+++ server/app.js
@@ -1,43 +1,46 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const db = require('./db')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const db = require('./db');
+const app = express();
+const PORT = 3000;
 //test
 // Logging middleware
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // Body parsing middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Static middleware
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/api/puppies', require('./api/puppies'));
 
 // If you want to add routes, they should go here!
 // You can either write them here, or organize them into
 // a separate router!
 
+
 // For all GET requests that aren't to an API route,
 // we will send the index.html!
 app.get('/*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
 
 // Handle 404s
 app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // Error handling endware
 app.use((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.send(err.message || 'Internal server error')
-})
+  res.status(err.status || 500);
+  res.send(err.message || 'Internal server error');
+});
 
 db.sync()
   .then(() => console.log('The database is synced'))
@@ -48,4 +51,4 @@ db.sync()
         http://localhost:3000/
       `)
     )
-  )
+  );

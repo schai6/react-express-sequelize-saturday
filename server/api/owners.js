891ca115a9417ca8aff34b58268a2adfec81594a
Sam Chai <sam.chai0501@gmail.com>
2018-05-05 15:46:43 -0400

    fix owners to use async await

--- server/api/owners.js
+++ server/api/owners.js
@@ -2,11 +2,13 @@ const db = require('../db');
 const Owner = db.model('owner');
 const router = require('express').Router();
 
-router.get('/api/owners', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const owners = Owner.findAll();
+    const owners = await Owner.findAll();
     res.json(owners);
-  } catch (err) { next(err); }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
